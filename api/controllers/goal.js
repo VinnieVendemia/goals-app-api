063339931f2bda7a39e4dc3f928dd3903d155c32
vvendemia <vvendemia1@gmail.com>
2017-04-03 17:32:11 -0400

    Add goal controller

---
+++ api/controllers/goal.js
@@ -0,0 +1,45 @@
+'use strict';
+// Include our "db"
+var db = require('../../config/db')();
+// Exports all the functions to perform on the db
+module.exports = {getAll, save, getOne, update, delGoal};
+
+//GET /goal operationId
+function getAll(req, res, next) {
+  res.json({ goals: db.find()});
+}
+//POST /goal operationId
+function save(req, res, next) {
+    res.json({success: db.save(req.body), description: "goal added to the list!"});
+}
+//GET /goal/{id} operationId
+function getOne(req, res, next) {
+    var id = req.swagger.params.id.value; //req.swagger contains the path parameters
+    var goal = db.find(id);
+    if(goal) {
+        res.json(goal);
+    }else {
+        res.status(204).send();
+    }       
+}
+//PUT /goal/{id} operationId
+function update(req, res, next) {
+    var id = req.swagger.params.id.value; //req.swagger contains the path parameters
+    var goal = req.body;
+    if(db.update(id, goal)){
+        res.json({success: 1, description: "goal updated!"});
+    }else{
+        res.status(204).send();
+    }
+
+}
+//DELETE /goal/{id} operationId
+function delGoal(req, res, next) {
+    var id = req.swagger.params.id.value; //req.swagger contains the path parameters
+    if(db.remove(id)){
+        res.json({success: 1, description: "goal deleted!"});
+    }else{
+        res.status(204).send();
+    }
+
+}
