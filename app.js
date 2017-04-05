417d1187e45d01c4c5225be9dc5f69e89bfb4acd
Vinnie Vendemia <vvendemia@jibe.com>
2017-04-05 10:55:20 -0400

    Add swagger-ui-express

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
 module.exports = app; // for testing
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 var config = {
   appRoot: __dirname // required config
 };
@@ -20,4 +23,6 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
   }
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 });
