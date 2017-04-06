79cf3a0debc37fbb56e1a8d24c8fe4bcb3d72b29
Vinnie Vendemia <vvendemia1@gmail.com>
2017-04-06 18:02:42 -0400

    Use swagger tools in app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 var app = require('express')();
 module.exports = app; // for testing
-
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
 
 var config = {
   appRoot: __dirname // required config
@@ -14,15 +12,12 @@ var config = {
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
+
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
   // install middleware
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
-
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 });
