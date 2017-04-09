0c3e49bab4a33273722333f01e81bef0d5c90a52
vvendemia <vvendemia1@gmail.com>
2017-04-08 21:31:28 -0400

    Add save function to user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,9 +2,15 @@
 // Include our "db"
 var db = require('../../config/db')();
 // Exports all the functions to perform on the db
-module.exports = {getAll};
+module.exports = {getAll, save};
 
 //GET /goal operationId
 function getAll(req, res, next) {
 	res.json([{ username: "NONE", password: "NONE"}]);
+}
+
+//POST /goal operationId
+function save(req, res, next) {
+	res.json({ success: true});
+    // res.json({success: db.save(req.body), description: "User added to the list!"});
 }
