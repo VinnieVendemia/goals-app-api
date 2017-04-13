9534043f14c1949322da172a08d8e5f3d88d698a
vvendemia <vvendemia1@gmail.com>
2017-04-13 18:04:03 -0400

    Add constructor, findAll and find functions to Goal

--- api/classes/Goal.js
+++ api/classes/Goal.js
@@ -1,6 +1,22 @@
 "use strict";
+var Db = require('./Db');
 
 class Goal extends Db {
+
+    constructor() {
+        super('goals');
+    }
+
+    findAll(cb) {
+        var query = super.selectAllQuery();
+        super.performQuery(query, {}, cb);
+    }
+
+    find(id, cb) {
+        var query = super.selectByKeyQuery('id', id);
+        super.performQuery(query, {id: id}, cb);
+    }
+
 }
 
 module.exports = Goal;
