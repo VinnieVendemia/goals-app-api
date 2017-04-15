5bfc2d38b9b0782235662306047609b568fb52ca
vvendemia <vvendemia1@gmail.com>
2017-04-15 13:52:21 -0400

    Add function find to User

--- api/classes/User.js
+++ api/classes/User.js
@@ -12,9 +12,14 @@ class User extends Db {
         super.performQuery(query, {}, cb);
     }
 
-		save(data, cb) {
+	save(data, cb) {
         var query = super.insertQuery('username, password, admin', ':username, :password, :admin')
         super.performQuery(query, data, cb);
+    }
+
+    find(id, cb) {
+        var query = super.selectByKeyQuery('id', id);
+        super.performQuery(query, {id: id}, cb);
     }
 }
 
