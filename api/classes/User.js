b8c4b263875fc6d5ad8c7b18572278d133ac2269
vvendemia <vvendemia1@gmail.com>
2017-04-15 11:40:05 -0400

    Add findAll function to User

--- api/classes/User.js
+++ api/classes/User.js
@@ -1,9 +1,15 @@
 "use strict";
+var Db = require('./Db');
 
 class User extends Db {
 
     constructor() {
         super('users');
+    }
+
+    findAll(cb) {
+        var query = super.selectAllQuery();
+        super.performQuery(query, {}, cb);
     }
 }
 
