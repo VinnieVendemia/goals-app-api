8aafaeb8378b17f159470e50934dc9815da629eb
vvendemia <vvendemia1@gmail.com>
2017-04-29 08:12:28 -1000

    Update configFactoryTest

--- tests/classes/configFactoryTest.js
+++ tests/classes/configFactoryTest.js
@@ -5,7 +5,11 @@ var ConfigFactory = require('../../api/classes/ConfigFactory');
 
 test('init test', function (t) {
    var dbConfig = new ConfigFactory();
+   var config = dbConfig.fetchDbConfig();
 
    t.ok(dbConfig, 'dbConfig should be OK');
+   t.ok(config, 'config should be OK');
+   t.equal(config['host'], 'localhost', 'HOST SHOULD BE LOCALHOST')
+
    t.end();
 });
