55a09a354fd8525abbb99fb56bbf613a7367af93
vvendemia <vvendemia1@gmail.com>
2017-04-30 10:44:26 -1000

    Add test for insertQuery

--- tests/classes/goalTest.js
+++ tests/classes/goalTest.js
@@ -7,5 +7,19 @@ test('init test', function (t) {
    var goal = new Goal();
 
    t.ok(goal, 'goal should be OK');
+
+
+   var insertQuery = goal.insertQuery('title', 'test_title')
+   t.equal(insertQuery, 'INSERT INTO goals (title) VALUES (test_title)')
+
+   t.end();
+});
+
+test('query test', function (t) {
+   var goal = new Goal();
+
+   var insertQuery = goal.insertQuery('title', 'test_title')
+   t.equal(insertQuery, 'INSERT INTO goals (title) VALUES (test_title)')
+
    t.end();
 });
