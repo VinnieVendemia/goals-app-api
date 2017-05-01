4516c5e24efcd7e43e6b0b1d7a630fe0955c742f
vvendemia <vvendemia1@gmail.com>
2017-05-01 09:25:52 -1000

    Add additional tests for Goal class

--- tests/classes/goalTest.js
+++ tests/classes/goalTest.js
@@ -15,11 +15,47 @@ test('init test', function (t) {
    t.end();
 });
 
-test('query test', function (t) {
+test('insert query test', function (t) {
    var goal = new Goal();
 
    var insertQuery = goal.insertQuery('title', 'test_title')
    t.equal(insertQuery, 'INSERT INTO goals (title) VALUES (test_title)')
+
+   t.end();
+});
+
+test('selectAll query test', function (t) {
+   var goal = new Goal();
+
+   var query = goal.selectAllQuery();
+   t.equal(query, 'SELECT * FROM goals')
+
+   t.end();
+});
+
+test('selectByKeyQuery query test', function (t) {
+   var goal = new Goal();
+
+   var query = goal.selectByKeyQuery('id', 1)
+   t.equal(query, 'SELECT * FROM goals where id = 1')
+
+   t.end();
+});
+
+test('deleteQuery query test', function (t) {
+   var goal = new Goal();
+
+   var query = goal.deleteQuery('id', 1)
+   t.equal(query, 'DELETE FROM goals where id = 1')
+
+   t.end();
+});
+
+test('updateQuery query test', function (t) {
+   var goal = new Goal();
+
+   var query = goal.updateQuery('title', 'id', 1)
+   t.equal(query, 'UPDATE goals SET title=:title where id = 1')
 
    t.end();
 });
