a7c5198f188c53083c4bb7e7ab4c7065a06f631c
vvendemia <vvendemia1@gmail.com>
2017-05-02 20:20:52 -0400

    Add additional test-cases to userTest

--- tests/classes/userTest.js
+++ tests/classes/userTest.js
@@ -9,3 +9,48 @@ test('init test', function (t) {
    t.ok(user, 'user should be OK');
    t.end();
 });
+
+test('insert query test', function (t) {
+   var user = new User();
+
+   var insertQuery = user.insertQuery('title', 'test_title')
+   t.equal(insertQuery, 'INSERT INTO users (title) VALUES (test_title)')
+
+   t.end();
+});
+
+test('selectAll query test', function (t) {
+   var user = new User();
+
+   var query = user.selectAllQuery();
+   t.equal(query, 'SELECT * FROM users')
+
+   t.end();
+});
+
+test('selectByKeyQuery query test', function (t) {
+   var user = new User();
+
+   var query = user.selectByKeyQuery('id', 1)
+   t.equal(query, 'SELECT * FROM users where id = 1')
+
+   t.end();
+});
+
+test('deleteQuery query test', function (t) {
+   var user = new User();
+
+   var query = user.deleteQuery('id', 1)
+   t.equal(query, 'DELETE FROM users where id = 1')
+
+   t.end();
+});
+
+test('updateQuery query test', function (t) {
+   var user = new User();
+
+   var query = user.updateQuery('title', 'id', 1)
+   t.equal(query, 'UPDATE users SET title=:title where id = 1')
+
+   t.end();
+});
