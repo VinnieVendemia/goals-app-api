cc521c505fc7b1edbf31191a48005a6c7655da5b
vvendemia <vvendemia1@gmail.com>
2017-05-11 20:34:57 -0400

    Add save function in progress controller

--- api/controllers/progress.js
+++ api/controllers/progress.js
@@ -3,16 +3,25 @@
 var logger   = global.logger.addLogger('[PROGRESS]');
 
 // Exports all the functions to perform on the db
-module.exports = {getAll};
+module.exports = {getAll, save};
 
 var Progress = require('../classes/Progress');
 var progress = new Progress();
 
-//GET /goal operationId
+//GET /progress operationId
 function getAll(req, res, next) {
   progress.findAll(
     function(data) {
       res.json({ progress: data});
     }
   );  
+}
+
+//POST /progress operationId
+function save(req, res, next) {
+    progress.save(req.body, 
+        function(data) {
+            res.json({success: 1, description: "progress added to the list!"})
+        }
+    )
 }
