b165d32fd0ef4e0f3b2a04adadb866849a0b9397
vvendemia <vvendemia1@gmail.com>
2017-05-12 16:25:00 -0400

    Add delProgress function to progress controller

--- api/controllers/progress.js
+++ api/controllers/progress.js
@@ -3,7 +3,7 @@
 var logger   = global.logger.addLogger('[PROGRESS]');
 
 // Exports all the functions to perform on the db
-module.exports = {getAll, save, getProgressByGoal};
+module.exports = {getAll, save, getProgressByGoal, delProgress};
 
 var Progress = require('../classes/Progress');
 var progress = new Progress();
@@ -31,4 +31,14 @@ function getProgressByGoal(req, res, next) {
     progress.findAllByGoal(id, function(data){
         res.json({ progress: data});
     });
+}
+
+//DELETE /goal/{id} operationId
+function delProgress(req, res, next) {
+    var id = req.swagger.params.id.value; //req.swagger contains the path parameters
+    progress.remove(id, 
+        function(data) {
+            res.json({success: 1, description: "progress deleted!"})
+        }
+    )
 }
