3d4755a43f3f5c6927fd0a1b349e4d37a4a63ef0
Vinnie Vendemia <vvendemia1@gmail.com>
2017-05-22 19:48:02 -0400

    Update auth middleware logic again

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -3,31 +3,39 @@ var ConfigFactory = require('../classes/ConfigFactory');
 
 module.exports.tokenMiddleWare = function (req, res, next) {
 
-  var authEndpoints = ['/goals', '/progress', '/users']
+  var blacklistEndpoints = ['/goals', '/progress', '/users']
+  var blacklistedRequest = false
 
-  for(var i = 0; i < authEndpoints.length; i++) {
-    if( req.path.includes(authEndpoints[i]) ) {
-      var token = req.headers['token']
-      var secret = new ConfigFactory().fetchSecretKey();
-
-      if(token) {
-        jwt.verify(token, secret, function(err, decoded) {      
-          if (err) {
-            return res.json({ success: false, message: 'Failed to authenticate token.' });    
-          } else {
-            // if everything is good, save to request for use in other routes
-            req.decoded = decoded
-            return next()
-          }
-        })
-      } else {
-        return res.status(403).send({ 
-            success: false, 
-            message: 'No token provided.' 
-        })
-      }
+  for(var i = 0; i < blacklistEndpoints.length; i++) {
+    if( req.path.includes(blacklistEndpoints[i]) ) {
+      blacklistedRequest = true
     }
   }
 
-  return next()
+  if( blacklistedRequest == true) {
+    var token = req.headers['token']
+    var secret = new ConfigFactory().fetchSecretKey();
+
+    if(token) {
+      jwt.verify(token, secret, function(err, decoded) {      
+        if (err) {
+          return res.status(401).send({ 
+            success: false, 
+            message: 'Unauthorized' 
+          })
+        } else {
+          // if everything is good, save to request for use in other routes
+          req.decoded = decoded
+          return next()
+        }
+      })
+    } else {
+      return res.status(403).send({ 
+          success: false, 
+          message: 'No token provided' 
+      })
+    }
+  } else {
+    return next()
+  }
 };
