4508ed388ac35ba8543f1f978de5409ff7bd17b8
Vinnie Vendemia <vvendemia1@gmail.com>
2017-05-22 19:26:31 -0400

    Update auth middleware by creating blacklist

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -3,28 +3,31 @@ var ConfigFactory = require('../classes/ConfigFactory');
 
 module.exports.tokenMiddleWare = function (req, res, next) {
 
-  if(req.path == "/authenticate") {
-    return next()
-  }else {
+  var authEndpoints = ['/goals', '/progress', '/users']
 
-    var token = req.headers['token']
-    var secret = new ConfigFactory().fetchSecretKey();
+  for(var i = 0; i < authEndpoints.length; i++) {
+    if( req.path.includes(authEndpoints[i]) ) {
+      var token = req.headers['token']
+      var secret = new ConfigFactory().fetchSecretKey();
 
-    if(token) {
-      jwt.verify(token, secret, function(err, decoded) {      
-        if (err) {
-          return res.json({ success: false, message: 'Failed to authenticate token.' });    
-        } else {
-          // if everything is good, save to request for use in other routes
-          req.decoded = decoded
-          return next()
-        }
-      })
-    } else {
-      return res.status(403).send({ 
-          success: false, 
-          message: 'No token provided.' 
-      })
+      if(token) {
+        jwt.verify(token, secret, function(err, decoded) {      
+          if (err) {
+            return res.json({ success: false, message: 'Failed to authenticate token.' });    
+          } else {
+            // if everything is good, save to request for use in other routes
+            req.decoded = decoded
+            return next()
+          }
+        })
+      } else {
+        return res.status(403).send({ 
+            success: false, 
+            message: 'No token provided.' 
+        })
+      }
     }
   }
+
+  return next()
 };
