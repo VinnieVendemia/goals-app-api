f6004e0d21372bdd32317c8bf85d066183eacc8a
Vinnie Vendemia <vvendemia1@gmail.com>
2017-05-25 18:52:22 -0400

    Add more progressTests

--- tests/classes/progressTest.js
+++ tests/classes/progressTest.js
@@ -14,3 +14,48 @@ test('init test', function (t) {
 
    t.end();
 });
+
+test('insert query test', function (t) {
+   var progress = new Progress();
+
+   var insertQuery = progress.insertQuery('title', 'test_title')
+   t.equal(insertQuery, 'INSERT INTO progress (title) VALUES (test_title)')
+
+   t.end();
+});
+
+test('selectAll query test', function (t) {
+   var progress = new Progress();
+
+   var query = progress.selectAllQuery();
+   t.equal(query, 'SELECT * FROM progress')
+
+   t.end();
+});
+
+test('selectByKeyQuery query test', function (t) {
+   var progress = new Progress();
+
+   var query = progress.selectByKeyQuery('id', 1)
+   t.equal(query, 'SELECT * FROM progress where id = 1')
+
+   t.end();
+});
+
+test('deleteQuery query test', function (t) {
+   var progress = new Progress();
+
+   var query = progress.deleteQuery('id', 1)
+   t.equal(query, 'DELETE FROM progress where id = 1')
+
+   t.end();
+});
+
+test('updateQuery query test', function (t) {
+   var progress = new Progress();
+
+   var query = progress.updateQuery('title', 'id', 1)
+   t.equal(query, 'UPDATE progress SET title=:title where id = 1')
+
+   t.end();
+});
